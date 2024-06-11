962615cc9c639ecc5a06188f698452aa726516bb
tzy <tangzy2021@gmail.com>
2024-06-11 18:41:40 +0800

    first commit

---
+++ src/views/CreateUserView.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+import axios from "axios";
+
+export default {
+  components: {},
+  data: () => ({
+    username: "",
+    email: "",
+    birthdate: "",
+  }),
+  methods: {
+    handleClick() {
+      axios.post("http://localhost:3000/users/create", this.$data);
+    },
+  },
+};
+</script>
+
+<template lang="pug">
+  div.flex.flex-col.items-center.gap-2
+    input.border.border-1.p-1.m-1(name="username" placeholder="username" v-model="username")
+    input.border.border-1.p-1.m-1(name="username" placeholder="username" v-model="email")
+    input.border.border-1.p-1.m-1(name="username" placeholder="username" v-model="birthdate")
+    button.border.border-1.p-1(@click="handleClick") click
+</template>
